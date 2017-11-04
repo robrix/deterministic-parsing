74aefab1dc6562ddd39252a4b58642dca4d31c45
Rob Rix <rob.rix@me.com>
2017-11-04 12:06:55 -0400

    Represent nullibility with a value to produce at the end of input.

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -17,70 +17,59 @@ type Noskip s = [[s]]
 
 type ParserCont s a = Input s -> Noskip s -> Either String (a, Input s)
 
-data Table s a
-  = Table
-    { tableBranches :: Map.Map s a
-    , tableDefault :: Maybe a
-    }
-  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
-
-instance Symbol s => Semigroup (Table s a) where
-  Table b1 d1 <> Table b2 d2 = Table (b1 <> b2) (d1 <|> d2)
-
-instance Symbol s => Monoid (Table s a) where
-  mempty = Table mempty Nothing
-  mappend = (<>)
+newtype Table s a = Table { tableBranches :: Map.Map s a }
+  deriving (Eq, Foldable, Functor, Monoid, Ord, Semigroup, Show, Traversable)
 
 data Parser s a
   = Parser
-    { parserNullible :: Bool
+    { parserNull :: Maybe a
     , parserFirst :: [s]
     , parserTable :: Table s (ParserCont s a)
     }
 
 parse :: Symbol s => Parser s a -> Input s -> Either String a
-parse (Parser _ _ table) input = do
-  (a, rest) <- choose table input []
+parse (Parser e _ table) input = do
+  (a, rest) <- choose e table input []
   case rest of
     [] -> Right a
     _ -> Left "no rule to match at end"
 
 instance Functor (Parser s) where
-  fmap g (Parser n f table) = Parser n f (fmap (fmap (fmap (first g)) .) table)
+  fmap g (Parser n f table) = Parser (fmap g n) f (fmap (fmap (fmap (first g)) .) table)
 
 instance Symbol s => Applicative (Parser s) where
-  pure a = Parser True [] (Table mempty (Just (\ i _ -> Right (a, i))))
+  pure a = Parser (Just a) [] (Table mempty)
 
-  Parser n1 f1 t1 <*> ~(Parser n2 f2 t2) = Parser (n1 && n2) (combine n1 f1 f2) (t1 `tseq` t2)
+  Parser n1 f1 t1 <*> ~(Parser n2 f2 t2) = Parser (n1 <*> n2) (combine n1 f1 f2) (t1 `tseq` t2)
     where t1 `tseq` t2
             = fmap (\ p input noskip -> do
               (f, input') <- p input (f2 : noskip)
-              (a, input'') <- choose t2 input' noskip
+              (a, input'') <- choose n2 t2 input' noskip
               pure (f a, input'')) t1
-            <> case tableDefault t1 of
+            <> case n1 of
               Just f -> fmap (\ q input noskip -> do
-                (f', input') <- f input noskip
-                (a, input'') <- q input' noskip
-                pure (f' a, input'')) t2
+                (a, input') <- q input noskip
+                pure (f a, input')) t2
               _ -> mempty
-          combine e s1 s2 = s1 `union` if e then s2 else []
+          combine (Just _) s1 s2 = s1 `union` s2
+          combine _        s1 _  = s1
 
-choose :: Symbol s => Table s (ParserCont s a) -> ParserCont s a
-choose (Table _ (Just a)) [] follow = a [] follow
-choose (Table _ Nothing) [] _ = Left "no rule to match at end"
-choose (Table b _) (c:cs) follow = case Map.lookup c b of
+choose :: Symbol s => Maybe a -> Table s (ParserCont s a) -> ParserCont s a
+choose (Just a) _ [] _ = Right (a, [])
+choose _ _ [] _ = Left "no rule to match at end"
+choose _ (Table b) (c:cs) follow = case Map.lookup c b of
   Just cont -> cont (c:cs) follow
   _ -> Left ("no rule to match " ++ show c)
 
 instance Symbol s => Alternative (Parser s) where
-  empty = Parser True [] (Table mempty Nothing)
+  empty = Parser Nothing [] (Table mempty)
 
-  Parser n1 f1 t1 <|> Parser n2 f2 t2 = Parser (n1 || n2) (f1 <> f2) (t1 <> t2)
+  Parser n1 f1 t1 <|> Parser n2 f2 t2 = Parser (n1 <|> n2) (f1 <> f2) (t1 <> t2)
 
 symbol :: s -> Parser s s
-symbol s = Parser False [s] (Table (Map.singleton s (\ inp _ -> case inp of
+symbol s = Parser Nothing [s] (Table (Map.singleton s (\ inp _ -> case inp of
   []      -> Left "unexpected eof"
-  (_:inp) -> Right (s, inp))) Nothing)
+  (_:inp) -> Right (s, inp))))
 
 
 data Expr = Lit Integer | Expr :+ Expr | Expr :* Expr
