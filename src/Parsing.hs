c5d4b379a15fe560db7eaeca468d6247a19e2cf0
Rob Rix <rob.rix@me.com>
2017-11-04 12:48:38 -0400

    Define Parser in continuation-passing style.

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -1,8 +1,7 @@
-{-# LANGUAGE ConstraintKinds, DeriveFoldable, DeriveFunctor, DeriveTraversable, FlexibleInstances, FunctionalDependencies, GeneralizedNewtypeDeriving, MultiParamTypeClasses #-}
+{-# LANGUAGE ConstraintKinds, DeriveFoldable, DeriveFunctor, DeriveTraversable, FlexibleInstances, FunctionalDependencies, GeneralizedNewtypeDeriving, MultiParamTypeClasses, RankNTypes #-}
 module Parsing where
 
 import Control.Applicative
-import Data.Bifunctor (first)
 import Data.Char
 import Data.Foldable
 import qualified Data.Map as Map
@@ -21,7 +20,7 @@ type Error = String
 type Result = Either Error
 type Success s a r = a -> State s -> r
 type Failure s a r = Error -> State s -> r
-type ParserCont s a = State s -> Noskip s -> Result (a, State s)
+type ParserCont s a r = State s -> Noskip s -> Success s a r -> Failure s a r -> r
 
 newtype Table s a = Table { tableBranches :: Map.Map s a }
   deriving (Eq, Foldable, Functor, Monoid, Ord, Semigroup, Show, Traversable)
@@ -30,46 +29,44 @@ data Parser s a
   = Parser
     { parserNull :: Maybe a
     , parserFirst :: Set.Set s
-    , parserTable :: Table s (ParserCont s a)
+    , parserTable :: forall r . Table s (ParserCont s a r)
     }
 
 parse :: Symbol s => Parser s a -> State s -> Result a
 parse (Parser e _ table) state = do
-  (a, rest) <- choose e table state []
+  (a, rest) <- choose e table state [] (curry Right) (const . Left)
   case rest of
     [] -> Right a
     _ -> Left "no rule to match at end"
 
 instance Functor (Parser s) where
-  fmap g (Parser n f table) = Parser (fmap g n) f (fmap (\ cont state -> fmap (first g) . cont state) table)
+  fmap g (Parser n f table) = Parser (fmap g n) f (fmap (\ cont state noskip yield err -> cont state noskip (yield . g) err) table)
 
 instance Symbol s => Applicative (Parser s) where
   pure a = Parser (Just a) mempty mempty
 
   Parser n1 f1 t1 <*> ~(Parser n2 f2 t2) = Parser (n1 <*> n2) (combine n1 f1 f2) (t1 `tseq` t2)
     where t1 `tseq` t2
-            = fmap (\ p state noskip -> do
-              (f, state') <- p state (f2 : noskip)
-              (a, state'') <- choose n2 t2 state' noskip
-              let fa = f a
-              fa `seq` pure (fa, state'')) t1
+            = fmap (\ p state noskip yield err ->
+              p state (f2 : noskip) (\ f state' ->
+                choose n2 t2 state' noskip (\ a state'' ->
+                  let fa = f a in fa `seq` yield fa state'') err) err) t1
             <> case n1 of
-              Just f -> fmap (\ q state noskip -> do
-                (a, state') <- q state noskip
-                let fa = f a
-                fa `seq` pure (fa, state')) t2
+              Just f -> fmap (\ q state noskip yield err ->
+                q state noskip (\ a state' ->
+                  let fa = f a in fa `seq` yield fa state') err) t2
               _ -> mempty
           combine (Just _) s1 s2 = s1 <> s2
           combine _        s1 _  = s1
 
-choose :: Symbol s => Maybe a -> Table s (ParserCont s a) -> ParserCont s a
-choose (Just a) _         []     _      = Right (a, [])
-choose _        _         []     _      = Left "no rule to match at end"
-choose nullible (Table b) (c:cs) noskip = fromMaybe notFound (Map.lookup c b) (c:cs) noskip
-  where notFound _ _
+choose :: Symbol s => Maybe a -> Table s (ParserCont s a r) -> ParserCont s a r
+choose (Just a) _         []     _      yield _   = yield a []
+choose _        _         []     _      _     err = err "no rule to match at end" []
+choose nullible (Table b) (c:cs) noskip yield err = fromMaybe notFound (Map.lookup c b) (c:cs) noskip yield err
+  where notFound _ _ _ _
           | Just a <- nullible
-          , any (c `Set.member`) noskip = Right (a, c:cs)
-          | otherwise                   = Left ("expected " ++ expected ++ " but got " ++ show c)
+          , any (c `Set.member`) noskip = yield a (c:cs)
+          | otherwise                   = err ("expected " ++ expected ++ " but got " ++ show c) (c:cs)
         expected = "(" ++ intercalate ", " (map show (Map.keys b)) ++ ")"
 
 instance Symbol s => Alternative (Parser s) where
@@ -78,9 +75,9 @@ instance Symbol s => Alternative (Parser s) where
   Parser n1 f1 t1 <|> Parser n2 f2 t2 = Parser (n1 <|> n2) (f1 <> f2) (t1 <> t2)
 
 symbol :: s -> Parser s s
-symbol s = Parser Nothing (Set.singleton s) (Table (Map.singleton s (\ inp _ -> case inp of
-  []      -> Left "unexpected eof"
-  (_:inp) -> Right (s, inp))))
+symbol s = Parser Nothing (Set.singleton s) (Table (Map.singleton s (\ inp _ yield err -> case inp of
+  []     -> err "unexpected eof" []
+  _:rest -> yield s rest)))
 
 
 data Expr = Lit Integer | Expr :+ Expr | Expr :* Expr
