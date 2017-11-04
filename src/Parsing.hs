330922d9e9f6ac8cd3563ed1a1160fd957e69806
Rob Rix <rob.rix@me.com>
2017-11-04 12:58:12 -0400

    Separate construction of tables from construction of maps.

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -2,6 +2,7 @@
 module Parsing where
 
 import Control.Applicative
+import Data.Bifunctor (second)
 import Data.Char
 import Data.Foldable
 import qualified Data.Map as Map
@@ -29,32 +30,33 @@ data Parser s a
   = Parser
     { parserNull :: Maybe a
     , parserFirst :: Set.Set s
-    , parserTable :: forall r . Table s (ParserCont s a r)
+    , parserTable :: forall r . [(s, ParserCont s a r)]
     }
 
 parse :: Symbol s => Parser s a -> State s -> Result a
 parse (Parser e _ table) state = do
-  (a, rest) <- choose e table state [] (curry Right) (const . Left)
+  (a, rest) <- choose e (Table (Map.fromList table)) state [] (curry Right) (const . Left)
   case rest of
     [] -> Right a
     _ -> Left "no rule to match at end"
 
 instance Functor (Parser s) where
-  fmap g (Parser n f table) = Parser (fmap g n) f (fmap (\ cont state noskip yield -> cont state noskip (yield . g)) table)
+  fmap g (Parser n f table) = Parser (fmap g n) f (fmap (second (\ cont state noskip yield -> cont state noskip (yield . g))) table)
 
 instance Symbol s => Applicative (Parser s) where
   pure a = Parser (Just a) mempty mempty
 
   Parser n1 f1 t1 <*> ~(Parser n2 f2 t2) = Parser (n1 <*> n2) (combine n1 f1 f2) (t1 `tseq` t2)
-    where t1 `tseq` t2
-            = fmap (\ p state noskip yield err ->
+    where table2 = Table (Map.fromList t2)
+          t1 `tseq` t2
+            = fmap (second (\ p state noskip yield err ->
               p state (f2 : noskip) (\ f state' ->
-                choose n2 t2 state' noskip (\ a state'' ->
-                  let fa = f a in fa `seq` yield fa state'') err) err) t1
+                choose n2 table2 state' noskip (\ a state'' ->
+                  let fa = f a in fa `seq` yield fa state'') err) err)) t1
             <> case n1 of
-              Just f -> fmap (\ q state noskip yield err ->
+              Just f -> fmap (second (\ q state noskip yield err ->
                 q state noskip (\ a state' ->
-                  let fa = f a in fa `seq` yield fa state') err) t2
+                  let fa = f a in fa `seq` yield fa state') err)) t2
               _ -> mempty
           combine (Just _) s1 s2 = s1 <> s2
           combine _        s1 _  = s1
@@ -77,9 +79,9 @@ instance Symbol s => Alternative (Parser s) where
   Parser n1 f1 t1 <|> Parser n2 f2 t2 = Parser (n1 <|> n2) (f1 <> f2) (t1 <> t2)
 
 symbol :: s -> Parser s s
-symbol s = Parser Nothing (Set.singleton s) (Table (Map.singleton s (\ inp _ yield err -> case inp of
+symbol s = Parser Nothing (Set.singleton s) [(s, \ inp _ yield err -> case inp of
   []     -> err "unexpected eof" []
-  _:rest -> yield s rest)))
+  _:rest -> yield s rest)]
 
 
 data Expr = Lit Integer | Expr :+ Expr | Expr :* Expr
