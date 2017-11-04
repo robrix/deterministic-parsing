a3b62ba935b89863a8124f70c052507396c0def4
Rob Rix <rob.rix@me.com>
2017-11-04 10:40:34 -0400

    Rename Det to Parser.

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -19,40 +19,40 @@ combine e s1 s2 = s1 `union` if e then s2 else []
 type Input s = [s]
 type Follow s = [s]
 
-type DetCont s a = Input s -> Follow s -> Either String (a, Input s)
+type ParserCont s a = Input s -> Follow s -> Either String (a, Input s)
 
-ppure :: a -> DetCont s a
+ppure :: a -> ParserCont s a
 ppure a i _ = Right (a, i)
 
-psymbol :: Symbol s => s -> DetCont s s
+psymbol :: Symbol s => s -> ParserCont s s
 psymbol s []      _ = Left "unexpected eof"
 psymbol s (_:inp) _ = Right (s, inp)
 
-data Det s a = Det { detNullible :: Bool, detFirst :: [s], runDet :: DetCont s a }
+data Parser s a = Parser { parserNullible :: Bool, parserFirst :: [s], runParser :: ParserCont s a }
 
-invokeDet :: Symbol s => Det s a -> Input s -> Either String a
-invokeDet (Det _ _ p) inp = case p inp [] of
+invokeParser :: Symbol s => Parser s a -> Input s -> Either String a
+invokeParser (Parser _ _ p) inp = case p inp [] of
   Left s        -> Left s
   Right (a, []) -> Right a
   Right _       -> Left "no rule to match at eof"
 
-instance Functor (Det s) where
-  fmap g (Det n f cont) = Det n f (fmap (fmap (first g)) . cont)
+instance Functor (Parser s) where
+  fmap g (Parser n f cont) = Parser n f (fmap (fmap (first g)) . cont)
 
-instance Symbol s => Applicative (Det s) where
-  pure = Det True [] . ppure
+instance Symbol s => Applicative (Parser s) where
+  pure = Parser True [] . ppure
 
-  Det n1 f1 p1 <*> ~(Det n2 f2 p2) = Det (n1 && n2) (combine n1 f1 f2) (p1 `pseq` p2)
+  Parser n1 f1 p1 <*> ~(Parser n2 f2 p2) = Parser (n1 && n2) (combine n1 f1 f2) (p1 `pseq` p2)
     where p1 `pseq` p2 = \ inp follow -> do
             let comb = combine n2
             (v1, inp1) <- p1 inp (f2 `comb` follow)
             (v2, inp2) <- p2 inp1 follow
             pure (v1 v2, inp2)
 
-instance Symbol s => Alternative (Det s) where
-  empty = Det True [] (\ _ _ -> Left "empty")
+instance Symbol s => Alternative (Parser s) where
+  empty = Parser True [] (\ _ _ -> Left "empty")
 
-  Det n1 f1 p1 <|> Det n2 f2 p2 = Det (n1 || n2) (f1 <> f2) (p1 `palt` p2)
+  Parser n1 f1 p1 <|> Parser n2 f2 p2 = Parser (n1 || n2) (f1 <> f2) (p1 `palt` p2)
     where p1 `palt` p2 = p
             where p [] follow
                     | n1        = p1 [] follow
@@ -65,5 +65,5 @@ instance Symbol s => Alternative (Det s) where
                     | n2, s `elem` follow = p2 inp follow
                     | otherwise           = Left ("unrecognized symbol " ++ show s)
 
-instance Symbol s => Parsing s (Det s) where
-  symbol s = Det False [s] (psymbol s)
+instance Symbol s => Parsing s (Parser s) where
+  symbol s = Parser False [s] (psymbol s)
