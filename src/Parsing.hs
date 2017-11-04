ef37e0183c1105d2d540d254847d6fe0e613bcb3
Rob Rix <rob.rix@me.com>
2017-11-04 15:05:33 -0400

    Label the integer & parens productions.

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -108,10 +108,10 @@ data Expr = Lit Integer | Expr :+ Expr | Expr :* Expr
   deriving (Eq, Ord, Show)
 
 lit :: Parser Char Expr
-lit = Lit . fst . foldr (\ d (v, p) -> (d * p + v, p * 10)) (0, 1) <$> some (asum (map (fmap (fromIntegral . digitToInt) . symbol) ['0'..'9']))
+lit = Lit . fst . foldr (\ d (v, p) -> (d * p + v, p * 10)) (0, 1) <$> some (asum (map (fmap (fromIntegral . digitToInt) . symbol) ['0'..'9'])) <?> "integer"
 
 parens :: Parser Char a -> Parser Char a
-parens a = symbol '(' *> a <* symbol ')'
+parens a = symbol '(' *> a <* symbol ')' <?> "parens"
 
 expr :: Parser Char Expr
 expr = term `chainl1` ((:+) <$ symbol '+')
