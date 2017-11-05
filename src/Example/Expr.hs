a091474deb1a23e7957f3154eb881aa57c195416
Rob Rix <rob.rix@me.com>
2017-11-05 11:19:29 -0500

    Parse + and * as tokens.

--- src/Example/Expr.hs
+++ src/Example/Expr.hs
@@ -5,6 +5,7 @@ import Text.Deterministic.Parser
 import Text.Parser.Char
 import Text.Parser.Combinators
 import Text.Parser.Token
+import Text.Parser.Token.Highlight
 
 data Expr = Lit Integer | Expr :+ Expr | Expr :* Expr
   deriving (Eq, Ord, Show)
@@ -13,10 +14,10 @@ lit :: Parser Char Expr
 lit = Lit <$> integer
 
 expr :: Parser Char Expr
-expr = term `chainl1` ((:+) <$ char '+') <?> "expr"
+expr = term `chainl1` ((:+) <$ token (highlight Operator (char '+'))) <?> "expr"
 
 term :: Parser Char Expr
-term = factor `chainl1` ((:*) <$ char '*') <?> "term"
+term = factor `chainl1` ((:*) <$ token (highlight Operator (char '*'))) <?> "term"
 
 factor :: Parser Char Expr
 factor = parens expr <|> lit
