3c30690ca356fc631403641c0e0091f76a4dd9c7
Rob Rix <rob.rix@me.com>
2017-11-05 09:19:28 -0500

    Spacing.

--- src/Data/Set/Symbolic.hs
+++ src/Data/Set/Symbolic.hs
@@ -10,6 +10,7 @@ data Set a = Set (a -> Bool)
 intersection :: Set a -> Set a -> Set a
 intersection (Set p1) (Set p2) = Set ((&&) <$> p1 <*> p2)
 
+
 instance Semigroup (Set a) where
   Set p1 <> Set p2 = Set ((||) <$> p1 <*> p2)
 
