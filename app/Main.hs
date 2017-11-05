716592528ee0e31a15ba656626e134d536d66ff0
Rob Rix <rob.rix@me.com>
2017-11-05 08:59:54 -0500

    Parse each passed-in file.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
+import Control.Monad
+import Data.Bifoldable
+import Data.Foldable
+import Example.Expr
+import Parsing
+import System.Environment
+
 main :: IO ()
-main = pure ()
+main = do
+  files <- getArgs
+  traverse_ (bitraverse_ (putStrLn . formatError) print . parse expr <=< readFile) files
