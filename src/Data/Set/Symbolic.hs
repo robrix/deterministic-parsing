3830fb1c871d9ad97501aded3d1d635cb1ee6f9b
Rob Rix <rob.rix@me.com>
2017-11-05 09:19:23 -0500

    Define Set intersection.

--- src/Data/Set/Symbolic.hs
+++ src/Data/Set/Symbolic.hs
@@ -1,10 +1,14 @@
 module Data.Set.Symbolic
 ( Set
+, intersection
 ) where
 
 import Data.Semigroup
 
 data Set a = Set (a -> Bool)
+
+intersection :: Set a -> Set a -> Set a
+intersection (Set p1) (Set p2) = Set ((&&) <$> p1 <*> p2)
 
 instance Semigroup (Set a) where
   Set p1 <> Set p2 = Set ((||) <$> p1 <*> p2)
