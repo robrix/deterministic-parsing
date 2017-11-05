c7a00a78f21a871b2c9d84eda459fd8e749895bb
Rob Rix <rob.rix@me.com>
2017-11-05 09:59:27 -0500

    Define Table union in terms of the keys.

--- src/Data/Table.hs
+++ src/Data/Table.hs
@@ -5,13 +5,23 @@ module Data.Table
 , singleton
 ) where
 
+import Data.Function (on)
+import Data.List (unionBy)
 import Data.Semigroup
 
 newtype Table i a = Table { toList :: [(i, a)] }
-  deriving (Eq, Foldable, Functor, Monoid, Ord, Semigroup, Show, Traversable)
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 fromList :: [(i, a)] -> Table i a
 fromList = Table
 
 singleton :: i -> a -> Table i a
 singleton i a = Table [(i, a)]
+
+
+instance Eq i => Semigroup (Table i a) where
+  Table t1 <> Table t2 = Table (unionBy ((==) `on` fst) t1 t2)
+
+instance Eq i => Monoid (Table i a) where
+  mempty = Table []
+  mappend = (<>)
