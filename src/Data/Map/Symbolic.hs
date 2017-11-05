379a83e6a98be882e283f8ae33bb101128c414d0
Rob Rix <rob.rix@me.com>
2017-11-05 10:01:02 -0500

    Define a Semigroup instance for symbolic maps.

--- src/Data/Map/Symbolic.hs
+++ src/Data/Map/Symbolic.hs
@@ -3,9 +3,14 @@ module Data.Map.Symbolic
 , lookup
 ) where
 
+import Control.Applicative
+import Data.Semigroup
 import Prelude hiding (lookup)
 
 newtype Map i a = Map (i -> Maybe a)
 
 lookup :: i -> Map i a -> Maybe a
 lookup i (Map m) = m i
+
+instance Semigroup (Map i a) where
+  Map p1 <> Map p2 = Map ((<|>) <$> p1 <*> p2)
