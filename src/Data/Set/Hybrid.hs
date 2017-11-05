22c0c225e38b35b5722e0e667cb0ec23edbd6f7b
Rob Rix <rob.rix@me.com>
2017-11-05 09:40:05 -0500

    Stub in a hybrid set datatype.

--- src/Data/Set/Hybrid.hs
+++ src/Data/Set/Hybrid.hs
@@ -1,1 +1,10 @@
-module Data.Set.Hybrid where
+module Data.Set.Hybrid
+( Set
+) where
+
+import qualified Data.Set as Set
+import qualified Data.Set.Symbolic as Sym
+
+data Set a
+  = Set (Set.Set a)
+  | Sym (Sym.Set a)
