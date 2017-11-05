ba72611cbb62965df8b773cd8c6fbef1b891b574
Rob Rix <rob.rix@me.com>
2017-11-05 09:29:38 -0500

    Define a Table datatype.

--- src/Data/Table.hs
+++ src/Data/Table.hs
@@ -1,1 +1,9 @@
-module Data.Table where
+module Data.Table
+( Table(toList)
+, fromList
+) where
+
+newtype Table i a = Table { toList :: [(i, a)] }
+
+fromList :: [(i, a)] -> Table i a
+fromList = Table
