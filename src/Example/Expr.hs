3e2e13c0b7a5f6170952dbaa2100c58715311a2b
Rob Rix <rob.rix@me.com>
2017-11-05 11:12:50 -0500

    Define lit using the integer token parser.

--- src/Example/Expr.hs
+++ src/Example/Expr.hs
@@ -1,8 +1,6 @@
 module Example.Expr where
 
 import Control.Applicative
-import Data.Char
-import Data.Foldable
 import Text.Deterministic.Parser
 import Text.Parser.Char
 import Text.Parser.Combinators
@@ -12,7 +10,7 @@ data Expr = Lit Integer | Expr :+ Expr | Expr :* Expr
   deriving (Eq, Ord, Show)
 
 lit :: Parser Char Expr
-lit = Lit . fst . foldr (\ d (v, p) -> (d * p + v, p * 10)) (0, 1) <$> some (asum (map (fmap (fromIntegral . digitToInt) . char) ['0'..'9'])) <?> "integer"
+lit = Lit <$> integer
 
 expr :: Parser Char Expr
 expr = term `chainl1` ((:+) <$ char '+') <?> "expr"
