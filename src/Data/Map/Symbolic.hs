833f6d748b7ef99cb47310a8fda6e53244c658dd
Rob Rix <rob.rix@me.com>
2017-11-05 09:26:06 -0500

    Define a lookup function.

--- src/Data/Map/Symbolic.hs
+++ src/Data/Map/Symbolic.hs
@@ -1,5 +1,11 @@
 module Data.Map.Symbolic
 ( Map
+, lookup
 ) where
 
+import Prelude hiding (lookup)
+
 newtype Map i a = Map (i -> Maybe a)
+
+lookup :: i -> Map i a -> Maybe a
+lookup i (Map m) = m i
