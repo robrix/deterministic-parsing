71362154bae8b2dc62c6461d113095b2edaaa0c9
Rob Rix <rob.rix@me.com>
2017-11-05 22:07:53 -0500

    Define a Token datatype.

--- src/Text/Deterministic/Token.hs
+++ src/Text/Deterministic/Token.hs
@@ -1,5 +1,10 @@
 module Text.Deterministic.Token where
 
+data Token s = Token
+  { tokenSymbol :: !s
+  , tokenDelta  :: {-# UNPACK #-} !Delta
+  }
+
 data Delta = Delta
   { deltaBytes   :: {-# UNPACK#-} !Int
   , deltaLines   :: {-# UNPACK#-} !Int
